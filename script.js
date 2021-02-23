fb111892f6dbbad2c709fffc5885a7aa946dd8e1
Dhroov Goel <dhroov19303@iiitd.ac.in>
2021-02-23 22:01:24 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,14 @@
+particlesJS.load('particles-js', 'particles.json', function() {
+    console.log('callback - particles.js config loaded');
+  });
+  
+var typed = new Typed('.type', {
+    strings: ["Developer.",
+              "Designer?",
+              "Sports Enthusiast."],
+    // smartBackspace: true // Default value
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true
+});
+
