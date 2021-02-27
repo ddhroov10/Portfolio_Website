8fcc18150b0d98590b4bb4212c2e2c8b58fe4bb5
Dhroov Goel <dhroov19303@iiitd.ac.in>
2021-02-28 00:32:05 +0530

    All github and youtube links added for game projects

--- script.js
+++ script.js
@@ -12,3 +12,34 @@ var typed = new Typed('.type', {
     loop: true
 });
 
+function PlayGameSound(soundobj, int_num) {
+  // document.write(int_num);
+  var thissound=document.getElementById(soundobj);
+  thissound.play();
+  
+  if(int_num == 1){
+    document.getElementById("game_project1").style.marginTop = "100px";
+  }
+  if(int_num == 2){
+    document.getElementById("game_project2").style.marginTop = "100px";
+  }
+  if(int_num == 3){
+    document.getElementById("game_project3").style.marginTop = "100px";
+  }
+  
+}
+
+function StopGameSound(soundobj, int_num) {
+  var thissound=document.getElementById(soundobj);
+  thissound.pause();
+  if(int_num == 1){
+    document.getElementById("game_project1").style.marginTop = "0px";
+  }
+  if(int_num == 2){
+    document.getElementById("game_project2").style.marginTop = "0px";
+  }
+  if(int_num == 3){
+    document.getElementById("game_project3").style.marginTop = "0px";
+  }
+  // thissound.currentTime = 0;
+}
